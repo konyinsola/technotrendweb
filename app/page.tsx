446b4dfcf9458f2a8b979d1a82549765a8b1d16a
Deyems <adeyemisola11@gmail.com>
2026-06-26 00:39:04 +0100

    refactored to take components a layer inside

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,9 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import Stack from "@/components/Stack";
-import About from "@/components/About";
-import Services from "@/components/Services";
-import CTA from "@/components/CTA";
-import Products from "@/components/Products";
-import TelecomProducts from "@/components/TelecomProducts";
-import Partners from "@/components/Partners";
-import Process from "@/components/Process";
-import Clients from "@/components/Clients";
-import Footer from "@/components/Footer";
+import HomePage from '@/components/pages/home-page';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#f8f6ef]">
-      <Navbar />
-      <Hero />
-      <Stack />
-      <About />
-      <Services />
-      <Clients />
-      <Products />
-      <TelecomProducts />
-      <Partners />
-      <Process />
-      <CTA />
-      <Footer />
+      <HomePage />
     </main>
   );
 }
